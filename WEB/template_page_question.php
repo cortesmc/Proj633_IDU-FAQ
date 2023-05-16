496d094362e6ded47099017bc0c291553154891b
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2023-05-16 22:47:07 +0200

    [ADD] add template page question

---
+++ WEB/template_page_question.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='template_page_question.css' />
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <H1>TITRE DU SITE</H1>
+    </header>
+    <div class='principal'>
+        <div class='container_question'>
+            <div class='question'>
+                <div class='haut_qst'>
+                    <div class='titre_sujet'>
+                        <h4>Sujet :</h4>
+                        <p>Ceci est un titre</p>
+                    </div>
+                    <div class='btn_like'>
+                        <input type='submit' value='❤'></input>
+                    </div>
+                </div>
+
+                <div class='texte_qst'>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magnam facere modi nobis repellat esse provident quas incidunt maxime necessitatibus sint molestiae magni beatae in suscipit officiis doloremque, harum perferendis!</p>
+                </div>
+            </div>
+            <div class='qst_button'>
+                <div class='btn_edit'>
+                    <input type='submit' value='EDIT'></input>
+                </div>
+                <div class='btn_valider'>
+                    <input type='submit' value='VALIDER'></input>
+                </div>
+            </div>
+        </div>
+        <div class ='container_response'>
+            <div class='response_title'>
+                <H2>Réponses : (nb_reponses)</H2>
+            </div>
+            <div class='btn_write'>
+                <input type='submit' value='Write answer'></input>
+            </div>
+            <div class='liste_response'>
+                <div class='response'>
+                    <div class='haut_qst'>
+                        <div class='titre_sujet'>
+                            <h4>Sujet :</h4>
+                            <p>Ceci est un titre</p>
+                        </div>
+                        <div class='btn_etit_response'>
+                            <input type='submit' value='EDIT'></input>
+                        </div>
+                    </div>
+                    <div class='texte_qst'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magnam facere modi nobis repellat esse provident quas incidunt maxime necessitatibus sint molestiae magni beatae in suscipit officiis doloremque, harum perferendis!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magnam facere modi nobis repellat esse provident quas incidunt maxime necessitatibus sint molestiae magni beatae in suscipit officiis doloremque, harum perferendis!</p>
+                    </div>
+                </div>
+
+                <div class='response'>
+                    <div class='haut_qst'>
+                        <div class='titre_sujet'>
+                            <h4>Sujet :</h4>
+                            <p>Ceci est un titre</p>
+                        </div>
+                        <div class='btn_etit_response'>
+                            <input type='submit' value='EDIT'></input>
+                        </div>
+                    </div>
+                    <div class='texte_qst'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magnam facere modi nobis repellat esse provident quas incidunt maxime necessitatibus sint molestiae magni beatae in suscipit officiis doloremque, harum perferendis!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magnam facere modi nobis repellat esse provident quas incidunt maxime necessitatibus sint molestiae magni beatae in suscipit officiis doloremque, harum perferendis!</p>
+                    </div>
+                </div>
+
+                <div class='response'>
+                    <div class='haut_qst'>
+                        <div class='titre_sujet'>
+                            <h4>Sujet :</h4>
+                            <p>Ceci est un titre</p>
+                        </div>
+                        <div class='btn_etit_response'>
+                            <input type='submit' value='EDIT'></input>
+                        </div>
+                    </div>
+                    <div class='texte_qst'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magnam facere modi nobis repellat esse provident quas incidunt maxime necessitatibus sint molestiae magni beatae in suscipit officiis doloremque, harum perferendis!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magnam facere modi nobis repellat esse provident quas incidunt maxime necessitatibus sint molestiae magni beatae in suscipit officiis doloremque, harum perferendis!</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
