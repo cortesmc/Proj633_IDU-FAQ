43776d55eadb657c61f668bf958b3bac13149740
lilcdx <113095135+lilcdx@users.noreply.github.com>
2023-05-21 19:08:30 +0200

    [FIX+ADD] page_all_questions.php fixed structure + first draft CSS

--- WEB/page_all_questions.php
+++ WEB/page_all_questions.php
@@ -12,28 +12,29 @@
         <h1>Titre du site</h1>
     </header>
     <div id='content'>
-        <div id='buttons'>
-            <div id='writeQ'>
-                <form action='' method='post'>
-                    <input type='submit' class='btn' id='writeBtn' value='Poser une question'>
+        <div id='ask_container' class='container'>
+            <form action='' method='post'>
+                <input type='submit' class='btn' id='writeBtn' value='Poser une question'>
+            </form>
+            <div>
+                <form action='' method='post'></form>
+                    <div id='writeHeader'>
+                        <span>Sujet</span>
+                        <input type='text'>
+                    </div>
+                    <textarea name='questionTA' id='TextAreaQuestion' cols='30' rows='10'></textarea>
+                    <div id='QBtns'>
+                        <input type='submit' class='btn' id='backBtn' value='Annuler'>
+                        <input type='submit' class='btn' id='sendBtn' value='Envoyer'>
+                    </div>
                 </form>
-                <div>
-                    <form action='' method='post'></form>
-                        <div id='writeHeader'>
-                            <span>Sujet</span>
-                            <input type='text'>
-                        </div>
-                        <textarea name='questionTA' id='TextAreaQuestion' cols='30' rows='10'></textarea>
-                        <div id='QBtns'>
-                            <input type='submit' class='btn' id='backBtn' value='Annuler'>
-                            <input type='submit' class='btn' id='sendBtn' value='Envoyer'>
-                        </div>
-                    </form>
-                </div>
-            </div>            
-            <div id='unvalidatedQ'>
-
             </div>
+        </div>    
+        <div id='unvalidated_container'>
+
+        </div>
+        
+        <div id='questions_container' class='container'>
             <div id=searchDiv>
                 <div id='searchBarDiv'>
                     <form action='' method='post'>
@@ -46,20 +47,20 @@
                     </form>
                 </div>
             </div>
-        </div>
-        <div id='allQuestions'>
             <!-- div avec toutes les questions : modifier php -->
-            <div class='question'>
-                <a href='' class='questionTitle'>Ceci est un titre</a>
-                <input type='submit' value='❤' class='like'></input>
-            </div>
-            <div class='question'>
-                <a href='' class='questionTitle'>Ceci est un titre</a>
-                <input type='submit' value='❤' class='like'></input>
-            </div>
-            <div class='question'>
-                <a href='' class='questionTitle'>Ceci est un titre</a>
-                <input type='submit' value='❤' class='like'></input>
+            <div id='allQuestions'>
+                <div class='question'>
+                    <a href='' class='questionTitle'>Ceci est un titre</a>
+                    <input type='submit' value='❤' class='like'></input>
+                </div>
+                <div class='question'>
+                    <a href='' class='questionTitle'>Ceci est un titre</a>
+                    <input type='submit' value='❤' class='like'></input>
+                </div>
+                <div class='question'>
+                    <a href='' class='questionTitle'>Ceci est un titre</a>
+                    <input type='submit' value='❤' class='like'></input>
+                </div>
             </div>
         </div>
     </div>
